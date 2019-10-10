f568f7002dea948bf91d62e1b328d6a4939bdff3
Reid Klarsfeld <reidklarsfeld@Reids-MacBook-Pro.local>
2019-10-10 10:32:36 -0700

    finished react code preview and tab

---
+++ src/components/ReactCodePreview.tsx
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import { PrismAsyncLight as SyntaxHighlighter } from 'react-syntax-highlighter';
+import jsx from 'react-syntax-highlighter/dist/esm/languages/prism/jsx';
+import { dark as style } from 'react-syntax-highlighter/dist/esm/styles/prism';
+import { formatter } from '../utils/formatter.util';
+import componentRender from '../utils/componentRender.util';
+import ReactComponentRender from '../utils/ReactCodeRender.util'
+import { ComponentInt, ComponentsInt } from '../utils/InterfaceDefinitions';
+
+SyntaxHighlighter.registerLanguage('jsx', jsx);
+type Props = {
+  focusComponent: ComponentInt;
+  components: ComponentsInt;
+};
+
+class ReactCodePreview extends Component<Props> {
+  render(): JSX.Element {
+    const focusComponent: ComponentInt = this.props.focusComponent;
+    const components: ComponentsInt = this.props.components;
+    return (
+      <div>
+        <SyntaxHighlighter
+          style={style}
+          language="javascript"
+          customStyle={{
+            background: 'transparent',
+            border: '2px solid #e0e0e0',
+            overflow: 'auto',
+            fontSize: '14px',
+            backgroundColor: '#262626',
+            borderRadius: '20px',
+            margin: '10px 0px 0px 0px',
+            height: '24vh',
+          }}>
+          {formatter(ReactComponentRender(focusComponent, components))}
+        </SyntaxHighlighter>
+      </div>
+    );
+  }
+}
+
+export default ReactCodePreview;
